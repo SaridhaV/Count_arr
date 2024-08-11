f58cd4fb4b8052626340d056c6c920eb24cb0bc4
SaridhaV <154538347+SaridhaV@users.noreply.github.com>
2024-08-11 09:56:20 +0530

    Update count.py

--- count.py
+++ count.py
@@ -1,9 +1,8 @@
 arr=list(map(int,input().split()))
 n=int(input())
-for i in arr:
-    n=n-1
-    if(n>0):
-        print(n)
+for i in range(len(arr)):
+    if(n>arr[i]):
+        print(arr[i])
 
     
     
